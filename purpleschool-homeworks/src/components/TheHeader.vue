2abd0d1795f58c264d6c5f897cd219677c53cb83
Dmitry Kuksinsky <kuksik@inbox.ru>
2025-05-01 11:32:49 +0300

    3-events done

---
+++ purpleschool-homeworks/src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TheScore from "./TheScore.vue";
+import {ref} from "vue";
+
+const score = ref(100)
+
+</script>
+
+<template>
+  <header>
+    <div>Запомни слово</div>
+    <TheScore :score="score" />
+  </header>
+
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 121px;
+}
+
+</style>
---
+++ purpleschool-homeworks/src/components/TheScore.vue
@@ -0,0 +1,41 @@
+<script setup>
+import TheHeart from "../assets/icons/TheHeart.vue";
+const {score} = defineProps({
+  score: Number
+})
+
+</script>
+
+<template>
+  <div class="score">
+    <div class="score-value">{{score}}</div>
+    <div class="score-icon"><TheHeart /></div>
+
+  </div>
+
+</template>
+
+<style scoped>
+.score {
+  display: flex;
+  align-items: center;
+  padding: 12px 24px;
+  border-radius: 36px;
+  background: #CCE8FF;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.15);
+}
+.score-value {
+  color: #008BFE;
+  font-size: 1rem;
+  font-weight: 700;
+  line-height: 24px;
+  text-align: center;
+  margin-right: 10px;
+}
+.score-icon {
+  width: 24px;
+  height: 24px;
+}
+
+
+</style>
